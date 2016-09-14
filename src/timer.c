6530d1c9bdf2ed74b94cc696d07d32a40035871f
Srdja Matovic <s.matovic@app26.de>
2016-09-14 04:23:06 +0200

    cleanup

--- src/timer.c
+++ src/timer.c
@@ -1,36 +1,37 @@
 /*
-  Name:         Zeta Dva
-  Description:  Amateur level chess engine
+  Name:         Zeta
+  Description:  Experimental chess engine written in OpenCL.
   Author:       Srdja Matovic <s.matovic@app26.de>
   Created at:   2011-01-15
-  Updated at:   2016-07-13
+  Updated at:   2016-09
   License:      GPL >= v2
 
   Copyright (C) 2011-2016 Srdja Matovic
 
-  Zeta Dva is free software: you can redistribute it and/or modify
+  Zeta is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 2 of the License, or
   (at your option) any later version.
 
-  Zeta Dva is distributed in the hope that it will be useful,
+  Zeta is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 */
 
-#include <stdio.h>     /* for malloc free */
+#include <stdio.h>      /* for malloc free */
 #include <string.h>     /* for string compare */ 
 #include <time.h>       /* for time measurent */
 #include <sys/time.h>   /* for gettimeofday */
 
-double get_time (void) 
+// get time in milli seconds
+double get_time(void) 
 {
   struct timeval t;
   gettimeofday (&t, NULL);
   return t.tv_sec*1000 + t.tv_usec/1000;
 }
-void get_time_string (char *string)
+void get_date_string(char *string)
 {
   time_t t;
   struct tm *local;
@@ -45,11 +46,10 @@ void get_time_string (char *string)
 
   memcpy (string, tempstring, sizeof(char)*255);
 }
-
-void fprinttime(FILE *file)
+void fprintdate(FILE *file)
 {
   char timestring[256];
-  get_time_string (timestring);
+  get_date_string (timestring);
   fprintf(file, "%s, ", timestring);
 }
 
--- src/timer.h
+++ src/timer.h
@@ -1,19 +1,19 @@
 /*
-  Name:         Zeta Dva
-  Description:  Amateur level chess engine
+  Name:         Zeta
+  Description:  Experimental chess engine written in OpenCL.
   Author:       Srdja Matovic <s.matovic@app26.de>
   Created at:   2011-01-15
-  Updated at:   2016-07-13
+  Updated at:   2016-09
   License:      GPL >= v2
 
   Copyright (C) 2011-2016 Srdja Matovic
 
-  Zeta Dva is free software: you can redistribute it and/or modify
+  Zeta is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 2 of the License, or
   (at your option) any later version.
 
-  Zeta Dva is distributed in the hope that it will be useful,
+  Zeta is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
@@ -27,8 +27,8 @@
 #include <sys/time.h>   /* for gettimeofday */
 
 double get_time (void);
-void get_time_string (char *string);
-void fprinttime(FILE *file);
+void get_date_string (char *string);
+void fprintdate(FILE *file);
 
 #endif /* TIMER_H_INCLUDED */
 
